aa1ec7e069940b1dc747427dea978dd25534a54b
sarathpk931 <sarathpk931@gmail.com>
2023-09-28 15:08:01 +0530

    12

---
+++ src/polyfills.ts
@@ -0,0 +1,8 @@
+// These are recommended for most applications.
+import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
+import 'zone.js/dist/zone';
+
+// Import additional polyfills as needed for your application.
+import 'core-js/es6/promise'; // For Promise support
+import 'core-js/es6/array'; // For Array.includes() support
